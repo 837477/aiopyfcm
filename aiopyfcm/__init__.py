615799ab202a55719cb3471969cea8ab7bafbf0f
837477 <8374770@gmail.com>
2024-10-25 17:24:18 +0900

    Ver 0.2.0

--- aiopyfcm/__init__.py
+++ aiopyfcm/__init__.py
@@ -22,7 +22,7 @@ from .errors import (
 __TITLE__ = 'aiopyfcm'
 __SUMMARY__ = 'aiopyfcm is Python Asynchronous FCM Push Controller'
 __URL__ = 'https://github.com/837477/aiopyfcm'
-__VERSION__ = "0.1.0"
+__VERSION__ = "0.2.0"
 __AUTHOR__ = '837477'
 __EMAIL__ = '8374770@gmail.com'
 __LICENSE__ = 'MIT License'
